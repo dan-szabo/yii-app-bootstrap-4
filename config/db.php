9ddae14c15368f48fc31ede99818bfdc5e60f4cb
Daniel Szabo <dszaboplus@gmail.com>
2018-01-30 20:23:20 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiiappmid',
+    'dsn' => 'mysql:host=localhost;dbname=yiiappbootstrap4',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
 ];
